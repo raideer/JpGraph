d1384ba548832064a194fd920a6ae0977876f56c
raideer <idmania1@gmail.com>
2018-03-06 13:44:24 +0200

    Updated to 4.2.0

--- lib/JpGraph.php
+++ lib/JpGraph.php
@@ -1,21 +1,24 @@
 <?php
-namespace JpGraph ;
+namespace JpGraph;
+
 class JpGraph {
-    static  $loaded = false ;
-    static  $modules = array();
+    static $loaded = false;
+    static $modules = [];
+
     static function load(){
         if(self::$loaded !== true){
             include_once __DIR__.'/JpGraph/src/jpgraph.php';
             self::$loaded = true ;
         }
     }
+
     static function module($moduleName){
         self::load();
-        if(!in_array($moduleName,self::$modules)){
+        if(!in_array($moduleName, self::$modules)){
             $path = __DIR__.'/JpGraph/src/jpgraph_'.$moduleName.'.php' ;
-            if(file_exists($path)){
+            if (file_exists($path)) {
                 include_once $path ;
-            }else{
+            } else {
                 throw new \InvalidArgumentException('The JpGraphs\'s module "'.$moduleName.'" does not exist');
             }
         }
